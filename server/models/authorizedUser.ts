4025ab96b3c6128570f602354d372d0ff5b47989
May Roussel <may@maybreak.com>
2021-03-08 14:52:55 -0600

    Add name to AuthorizedUser

--- server/models/authorizedUser.ts
+++ server/models/authorizedUser.ts
@@ -3,6 +3,9 @@ import { Column, CreatedAt, Model, Table, UpdatedAt } from 'sequelize-typescript
 @Table
 export class AuthorizedUser extends Model {
   @Column
+  name: string;
+  
+  @Column
   profileId: string;
   
   @CreatedAt
